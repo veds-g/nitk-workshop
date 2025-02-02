29472946b048209b425632f67b8c45692ec2cefa
veds-g <guptavedant2312@gmail.com>
2025-02-02 12:17:28 +0530

    updating script
    Signed-off-by: veds-g <guptavedant2312@gmail.com>

--- post_data.sh
+++ post_data.sh
@@ -3,6 +3,6 @@
 for i in {1..600}
 do
   timestamp=$(date +%s)$(printf "%03d" $(($(date +%N) / 1000000)))
-  curl -kq -X POST -H "x-numaflow-event-time: $timestamp" -d "101 103 105 107" https://localhost:8444/vertices/in
+  curl -kq -X POST -H "x-numaflow-event-time: $timestamp" -d "Hi how are you" https://localhost:8444/vertices/in
   sleep 1
 done
